420aed4ccfbe5154a0539d8368c77991a0871e99
Orlof <orlof@users.noreply.github.com>
2016-03-01 00:35:16 +0200

    register panel

---
+++ src/org/megastage/emulator/DebugData.java
@@ -0,0 +1,69 @@
+package org.megastage.emulator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DebugData {
+    public HashMap<String, String> defines = new HashMap<>();
+    public HashMap<String, String> labels = new HashMap<>();
+    public List<LineData> lines = new ArrayList<>(65536);
+    public List<LineData> memory = new ArrayList<>(65536);
+
+    public static DebugData load(String filename) throws IOException {
+        DebugData me = new DebugData();
+
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        me.init(br);
+
+        return me;
+    }
+
+    private void init(BufferedReader br) throws IOException {
+        defines = initMap(br);
+        labels = initMap(br);
+
+        String filename = br.readLine();
+        while(filename != null) {
+            lines.add(new LineData(filename, br));
+            filename = br.readLine();
+        }
+    }
+
+    private HashMap<String, String> initMap(BufferedReader br) throws IOException {
+        int numItems = Integer.parseInt(br.readLine());
+
+        HashMap<String, String> map = new HashMap<>();
+        for(int i=0; i < numItems; i++) {
+            String key = br.readLine();
+            String val = br.readLine();
+            map.put(key, val);
+        }
+        return map;
+    }
+
+    public static class LineData {
+        public String filename;
+        public int lineNum;
+        public String text;
+        public List<Integer> mem;
+
+        public LineData(String filename, BufferedReader br) throws IOException {
+            this.filename = filename;
+            lineNum = Integer.parseInt(br.readLine()) + 1;
+
+            int len = Integer.parseInt(br.readLine());
+
+            mem = new ArrayList<>(len);
+            for(int i=0; i < len; i++) {
+                mem.add(Integer.parseInt(br.readLine(), 16));
+                br.readLine();
+            }
+
+            text = br.readLine();
+        }
+    }
+}
