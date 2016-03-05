5b94f80dc9f9dd89b5fe287efdc1ba086f673b46
Orlof <orlof@users.noreply.github.com>
2016-03-05 15:26:53 +0200

    emulator working again

--- src/org/megastage/emulator/LEM1802Viewer.java
+++ src/org/megastage/emulator/LEM1802Viewer.java
@@ -1,12 +1,6 @@
 package org.megastage.emulator;
 
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
@@ -16,10 +10,11 @@ import java.util.TimerTask;
 public class LEM1802Viewer {
     public LEMCanvas canvas = new LEMCanvas();
     public VirtualMonitor vm;
-    BufferedImage img2 = new BufferedImage(144, 112, 2);
-    BufferedImage img = new BufferedImage(128, 112, 2);
-    int[] pixels = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
+    BufferedImage img2;
+    BufferedImage img;
+    int[] pixels;
     protected boolean keepAlive = true;
+    GraphicsConfiguration config;
 
     public LEM1802Viewer() {
         canvas.setPreferredSize(new Dimension(144, 112));
@@ -27,6 +22,19 @@ public class LEM1802Viewer {
         canvas.setMaximumSize(new Dimension(144, 112));
         canvas.setFocusable(true);
         canvas.requestFocus();
+
+        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice device = env.getDefaultScreenDevice();
+        config = device.getDefaultConfiguration();
+
+        img2 = fuck(144, 112, 2);
+        img = fuck(128, 112, 2);
+        pixels = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
+
+    }
+
+    public BufferedImage fuck(int w, int h, int x) {
+        return config.createCompatibleImage(w, h, Transparency.TRANSLUCENT);
     }
 
     public VirtualMonitor attach(VirtualMonitor vm) {
@@ -72,25 +80,24 @@ public class LEM1802Viewer {
 
                     if (vm != null) {
                         vm.setPixels(pixels);
-                        for (int i = 0; i < 1000; i++) {
-                            vm.render();
-                        }
+                        vm.render();
                         int iScale = (getWidth() - 1) / 144 + 1;
                         iScale = (getHeight() - 1) / 112 + 1 < iScale ? (getHeight() - 1) / 112 + 1 : iScale;
                         iScale = iScale > 0 ? iScale : 1;
-                        img2 = new BufferedImage(144 * iScale, 112 * iScale, 2);
+                        img2 = fuck(144 * iScale, 112 * iScale, 2);
                         Graphics2D g = (Graphics2D) img2.getGraphics();
                         if (g != null) {
                             g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
-                            g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+                            g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED);
                             g.setColor(new Color(pixels[0x3000]));
                             g.fillRect(0, 0, 144 * iScale, 112 * iScale);
                             g.drawImage(img, 8 * iScale, 8 * iScale, 128 * iScale, 112 * iScale, null);
                             g.dispose();
                         }
+
                         if (bkG != null) {
-                            bkG.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-                            bkG.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+                            bkG.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+                            bkG.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED);
                             float scale;
                             if ((float) getWidth() / (float) getHeight() > 144f / 112f) {
                                 scale = (float) getHeight() / 112f;
